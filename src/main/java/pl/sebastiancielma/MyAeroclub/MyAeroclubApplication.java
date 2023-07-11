ee068fc5bbe3cf06e2650749a4516caf6de9d82d
Sebastian Cielma <cielmasebastian@gmail.com>
2023-07-11 12:58:35 +0200

    Initial commit

---
+++ src/main/java/pl/sebastiancielma/MyAeroclub/MyAeroclubApplication.java
@@ -0,0 +1,13 @@
+package pl.sebastiancielma.MyAeroclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAeroclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAeroclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sebastiancielma/MyAeroclub/MyAeroclubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sebastiancielma.MyAeroclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAeroclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
